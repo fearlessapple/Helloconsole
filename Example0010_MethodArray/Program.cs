7890473b96261d0ae6d8e32d78eb165e9338d048
Stas <smaster950@gmail.com>
2022-10-05 00:09:00 +0300

    Add Example0010_MethodArray project

---
+++ Example0010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 14746, 322, 563, 3564, 785, 687, 563, 386 };
+
+int n = array.Length;
+int find = 563;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
